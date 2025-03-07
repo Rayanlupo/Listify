6ed934c6b47b810a67272f0be6abdc06c9c8ee01
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2025-03-07 22:17:30 +0000

    IT WORKS! without gradient BUT IT WORKS!

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
+import figlet from "figlet";
 import inquirer from "inquirer";
-
-
+  
 async function listify(){
+    
+    console.log(figlet.textSync("Make It list!!!"));
     const answers = await inquirer.prompt({
         name:'stringInput',
         type :'input',
         message:  'Whats the string that you want to listify?',
-    })
+    });
     let string = answers.stringInput;
     
     const list = string.toString().split('');
-    console.log(list);
+    console.log(`[${list.join(", ")}]`);
 }
 
-await listify();
+listify();
