b16b8c5690ee40a9ae5aa9c8c6488e3b3bb0a135
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2025-03-08 10:52:35 +0000

    Ready to publish

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
 import figlet from "figlet";
 import inquirer from "inquirer";
-// need to ask if the user wants to keep the whitespaces or not
+import { input, confirm } from '@inquirer/prompts';
+console.clear()
+
 async function listify(){
-    
     console.log(figlet.textSync("Make It list!!!"));
     const answers = await inquirer.prompt({
         name:'stringInput',
         type :'input',
         message:  'Whats the string that you want to listify?',
     });
+    const allowSpaces = await confirm({ message: 'Do you want to keep the whitespaces?' });
+console.log(allowSpaces)
     let string = answers.stringInput;
-    
+    if (!allowSpaces) {
+        string = string.replace(/\s/g, "");
+    }
     const list = string.toString().split('');
-    console.log(`[${list.join(", ")}]`);
+   
+    console.log(JSON.stringify(list));
 }
 
 listify();
